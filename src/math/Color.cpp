fe5a224a7b8e39f209efe4909f02c8318ca6b0f7
KR-jaju <wnwoals0127@gmail.com>
2023-10-16 04:17:17 +0900

    Fix: Color int cast operator bug fix

--- src/math/Color.cpp
+++ src/math/Color.cpp
@@ -10,10 +10,10 @@ Color::Color(float r, float g, float b, float a) {
 }
 
 Color::operator	int() const {
-	int const	r = static_cast<int>(roundf(this->r * 255 + 0.5f)) & 256;
-	int const	g = static_cast<int>(roundf(this->g * 255 + 0.5f)) & 256;
-	int const	b = static_cast<int>(roundf(this->b * 255 + 0.5f)) & 256;
-	int const	a = static_cast<int>(roundf(this->a * 255 + 0.5f)) & 256;
+	int const	r = static_cast<int>(roundf(this->r * 255)) & 0xff;
+	int const	g = static_cast<int>(roundf(this->g * 255)) & 0xff;
+	int const	b = static_cast<int>(roundf(this->b * 255)) & 0xff;
+	int const	a = static_cast<int>(roundf(this->a * 255)) & 0xff;
 	return (a << 24 | b << 16 | g << 8 | r);
 }
 
