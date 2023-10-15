be4b0a7fd441d57a57399f17e2c9d13864819bc1
KR-jaju <wnwoals0127@gmail.com>
2023-10-15 23:30:15 +0900

    Feat: much more Vector3 functions

--- include/math/Vector3.hpp
+++ include/math/Vector3.hpp
@@ -10,7 +10,17 @@ public:
 	Vector3();
 	Vector3(float x, float y, float z);
 	Vector3(const Vector3 &ref);
-	Vector3& operator=(const Vector3 &ref);
+	Vector3	&operator=(const Vector3 &ref);
+	Vector3	operator+(const Vector3 &ref) const;
+	Vector3	operator-(const Vector3 &ref) const;
+	Vector3	operator-() const;
+	Vector3	operator*(float f) const;
+	float	operator*(const Vector3 &ref) const;
+	Vector3	operator/(float f) const;
+	Vector3	cross(Vector3 const &ref) const;
+	float	lengthSqr() const;
+	float	length() const;
+	Vector3	normalized() const;
 };
 
 #endif
--- src/math/Vector3.cpp
+++ src/math/Vector3.cpp
@@ -1,5 +1,6 @@
 
 #include "math/Vector3.hpp"
+#include <cmath>
 
 Vector3::Vector3() {
 	this->x = 0;
@@ -24,5 +25,72 @@ Vector3& Vector3::operator=(const Vector3 &ref) {
 	this->y = ref.y;
 	this->z = ref.z;
 	return *this;
+}
+
+Vector3	Vector3::operator+(const Vector3 &ref) const {
+	Vector3	ret;
+
+	ret.x = this->x + ref.x;
+	ret.y = this->y + ref.y;
+	ret.z = this->z + ref.z;
+	return (ret);
+}
+
+Vector3	Vector3::operator-(const Vector3 &ref) const {
+	Vector3	ret;
+
+	ret.x = this->x - ref.x;
+	ret.y = this->y - ref.y;
+	ret.z = this->z - ref.z;
+	return (ret);
+}
+
+Vector3	Vector3::operator-() const {
+	return (Vector3(-this->x, -this->y, -this->z));
+}
+
+Vector3	Vector3::operator*(float f) const {
+	Vector3	ret;
+
+	ret.x = this->x * f;
+	ret.y = this->y * f;
+	ret.z = this->z * f;
+	return (ret);
+}
+
+float	Vector3::operator*(const Vector3 &ref) const {
+	return (this->x * ref.x + this->y * ref.y + this->z * ref.z);
+}
+
+Vector3	Vector3::operator/(float f) const {
+	Vector3	ret;
+
+	ret.x = this->x / f;
+	ret.y = this->y / f;
+	ret.z = this->z / f;
+	return (ret);
+}
+
+Vector3	Vector3::cross(Vector3 const &ref) const {
+	Vector3	ret;
+
+	ret.x = this->y * ref.z - this->z * ref.y;
+	ret.y = this->z * ref.x - this->x * ref.z;
+	ret.z = this->x * ref.y - this->y * ref.x;
+	return (ret);
+}
+
+float	Vector3::lengthSqr() const {
+	return (*this * *this);
+}
+
+float	Vector3::length() const {
+	return (sqrtf(this->lengthSqr()));
+}
+
+Vector3	Vector3::normalized() const {
+	Vector3	ret(*this);
+
+	return (ret / ret.length());
 }
 
