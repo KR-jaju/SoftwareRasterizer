2b5ceda036f0fa3f006e5b940fd7e2ef8a6902ea
KR-jaju <wnwoals0127@gmail.com>
2023-10-16 04:18:56 +0900

    Refactor: StandardRasterizer refactor

--- include/rasterizer/StandardRasterizer.hpp
+++ include/rasterizer/StandardRasterizer.hpp
@@ -6,17 +6,17 @@
 
 class StandardRasterizer : public Rasterizer
 {
-	private:
-		int	width;
-		int height;
-		int *color;
-		float *depth;
-		void drawTriangle(Vertex &a, Vertex &b, Vertex &c);
-	public:
-		StandardRasterizer(int width, int height);
-		virtual ~StandardRasterizer();
-		void draw(Mesh &mesh, int count);
-		void blit(int *dst);
+private:
+	int		width;
+	int		height;
+	int		*color;
+	float	*depth;
+	void	drawTriangle(Vertex &a, Vertex &b, Vertex &c);
+public:
+	StandardRasterizer(int width, int height);
+	virtual ~StandardRasterizer();
+	void	draw(Mesh &mesh, int count);
+	void	blit(int *dst);
 };
 
 #endif
