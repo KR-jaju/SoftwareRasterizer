cab3f9838d0a991e253798c6e9a84dcc693a51b8
KR-jaju <wnwoals0127@gmail.com>
2023-10-16 15:34:59 +0900

    Feat: DefaultShader now has v, p matrices

--- include/rasterizer/DefaultShader.hpp
+++ include/rasterizer/DefaultShader.hpp
@@ -3,11 +3,15 @@
 # define DEFAULT_SHADER_HPP
 
 # include "rasterizer/Shader.hpp"
+# include "math/Matrix4x4.hpp"
 
 class DefaultShader : public Shader {
 private:
-
+	Matrix4x4	view;
+	Matrix4x4	projection;
 public:
+	void	setViewMatrix(Matrix4x4 &m);
+	void	setProjectionMatrix(Matrix4x4 &m);
 	void	vertex(Vertex const &in, Vertex &out);
 	void	fragment(Vertex const &in, int &color);
 };
--- src/rasterizer/DefaultShader.cpp
+++ src/rasterizer/DefaultShader.cpp
@@ -2,12 +2,22 @@
 #include "rasterizer/DefaultShader.hpp"
 
 void	DefaultShader::vertex(Vertex const &in, Vertex &out) {
-	(void) in;
-	(void) out;
-	out = in;
+	//out.position = this->view * in.position;
+	out.position = this->projection * (this->view * in.position);
+	out.normal = in.normal;
 }
 
 void	DefaultShader::fragment(Vertex const &in, int &color) {
-	color = Color(0.0, 1.0, 1.0, 0.0);
+	// color = Color(0.0, 1.0, 1.0, 0.0);
+	color = (Color)in.normal;
 	(void)in;
 }
+
+void	DefaultShader::setViewMatrix(Matrix4x4 &m) {
+	this->view = m;
+}
+
+void	DefaultShader::setProjectionMatrix(Matrix4x4 &m) {
+	this->projection = m;
+}
+
