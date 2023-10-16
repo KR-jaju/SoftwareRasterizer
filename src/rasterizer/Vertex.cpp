4f1bc3c7faf59c5f63c24aed8907f57bd677842e
KR-jaju <wnwoals0127@gmail.com>
2023-10-16 20:50:58 +0900

    Fix: mix for non perspective and perspective attributes

--- src/rasterizer/Vertex.cpp
+++ src/rasterizer/Vertex.cpp
@@ -2,10 +2,17 @@
 #include "rasterizer/Vertex.hpp"
 
 
-Vertex	Vertex::mix(Vertex const &a, Vertex const &b, Vertex const &c, float ratio_a, float ratio_b, float ratio_c) {
+Vertex	Vertex::mix(Vertex const &a, Vertex const &b, Vertex const &c, float u, float v, float w) {
+	float	pu = u / a.position.w;
+	float	pv = v / b.position.w;
+	float	pw = w / c.position.w;
+	float	pa = pu + pv + pw;
 	Vertex	ret;
 
-	ret.position = a.position * ratio_a + b.position * ratio_b + c.position * ratio_c;
-	ret.normal = a.normal * ratio_a + b.normal * ratio_b + c.normal * ratio_c;
+	pu /= pa;
+	pv /= pa;
+	pw /= pa;
+	ret.position = a.position * u + b.position * v + c.position * w;
+	ret.normal = a.normal * pu + b.normal * pv + c.normal * pw;
 	return (ret);
 }
