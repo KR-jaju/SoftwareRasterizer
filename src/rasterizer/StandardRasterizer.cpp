3660ac685c47c579af55f43656394deb23ddf4b4
son556 <shhmun02@naver.com>
2023-10-23 15:03:10 +0900

    Feat:standardrasterizer

--- src/rasterizer/StandardRasterizer.cpp
+++ src/rasterizer/StandardRasterizer.cpp
@@ -4,21 +4,24 @@
 #include <vector>
 #include <iostream>
 
-int min(int x1, int x2)
-{
-	if (x1 < x2)
-		return x1;
-	return x2;
-}
+// static
+// int min(int x1, int x2)
+// {
+// 	if (x1 < x2)
+// 		return x1;
+// 	return x2;
+// }
 
-int max(int x1, int x2)
-{
-	if (x1 > x2)
-		return x1;
-	return x2;
-}
+// static
+// int max(int x1, int x2)
+// {
+// 	if (x1 > x2)
+// 		return x1;
+// 	return x2;
+// }
 
-static bool cmp(Vertex &v1, Vertex &v2)
+static
+bool cmp(Vertex &v1, Vertex &v2)
 {
 	if (v1.position.y < v2.position.y)
 		return false;
@@ -57,9 +60,106 @@ int roundFloat(float x)
 	return int(x);
 }
 
+static
+std::vector<Vector4> Brasenham(Vector4 p1, Vector4 p2)
+{
+	std::vector<Vector4> res;
+	int	dx;
+	int	dy;
+	const int inc_x = (p1.x > p2.x ? -1 : 1);
+	const int inc_y = (p1.y > p2.y ? -1 : 1);
+	Vector4 tmp;
+
+	dx = roundFloat(abs(p1.x - p2.x));
+	dy = roundFloat((abs(p1.y - p2.y)));
+	if (dy <= dx)
+	{
+		int p = 2 * dy - dx;
+		int y = roundFloat(p1.y);
+		res.push_back(Vector4(roundFloat(p1.x), y, 0, 0));
+		if (p1.x <= p2.x)
+		{
+			for (int x = roundFloat(p1.x); x <= roundFloat(p2.x); x += inc_x)
+			{
+				if (0 >= p)
+					p += 2 * dy;
+				else
+				{
+					p += 2 * dy - 2 * dx;
+					y += inc_y;
+				}
+				if (roundFloat(res[res.size() - 1].y) == y)
+					res.pop_back();
+				res.push_back(Vector4(x, y, 0, 0));
+			}
+		}
+		else
+		{
+			for (int x = roundFloat(p1.x); x >= roundFloat(p2.x); x += inc_x)
+			{
+				if (0 >= p)
+					p += 2 * dy;
+				else
+				{
+					p += 2 * dy - 2 * dx;
+					y += inc_y;
+				}
+				if (roundFloat(res[res.size() - 1].y) == y)
+					res.pop_back();
+				res.push_back(Vector4(x, y, 0, 0));
+			}
+		}
+	}
+	else
+	{
+		int p = 2 * dy - dx;
+		int x = roundFloat(p1.x);
+		res.push_back(Vector4(x, roundFloat(p1.y), 0, 0));
+		if (p1.y <= p2.y)
+		{
+			for (int y = roundFloat(p1.y); y <= roundFloat(p2.y); y += inc_y)
+			{
+				if (0 >= p)
+					p += 2 * dx;
+				else
+				{
+					p += 2 * dx - 2 * dy;
+					x += inc_x;
+				}
+				res.push_back(Vector4(x, y, 0, 0));
+			}
+		}
+		else
+		{
+			for (int y = roundFloat(p1.y); y >= roundFloat(p2.y); y += inc_y)
+			{
+				if (0 >= p)
+					p += 2 * dx;
+				else
+				{
+					p += 2 * dx - 2 * dy;
+					x += inc_x;
+				}
+				res.push_back(Vector4(x, y, 0, 0));
+			}
+		}
+	}
+	return res;
+}
+
+static
+Vector4 findInterpolation(Vector4 &a, Vector4 &c, Vector4 &b)
+{
+	float t = (b.y - a.y) / (c.y - a.y);
+	float x = t * c.x + (1 - t) * a.x;
+	return Vector4(x, b.y, 0, 0);
+}
+
 void StandardRasterizer::drawTriangle(Vertex &a, Vertex &b, Vertex &c, Shader *shader)
 {
 	std::vector<Vertex> vec;
+	std::vector<Vector4> upTrianglePixelsLeft, upTrianglePixelsRight;
+	std::vector<Vector4> downTrianglePixelsLeft, downTrianglePixelsRight;
 	vec.push_back(a);
 	vec.push_back(b);
 	vec.push_back(c);
@@ -68,53 +168,206 @@ void StandardRasterizer::drawTriangle(Vertex &a, Vertex &b, Vertex &c, Shader *s
 	Vector4 tmp_a = toScreenSpace(vec[0].position, this->width, this->height);
 	Vector4 tmp_b = toScreenSpace(vec[1].position, this->width, this->height);
 	Vector4 tmp_c = toScreenSpace(vec[2].position, this->width, this->height);
-	float slope1 = (tmp_a.x - tmp_b.x) / (tmp_a.y - tmp_b.y);
-	float slope2 = (tmp_a.x - tmp_c.x) / (tmp_a.y - tmp_c.y);
-
-	float tmpX = tmp_a.x;
-	float tmpX2 = tmpX;
-
+	Vector4 tmp_d = findInterpolation(tmp_a, tmp_c, tmp_b);
 	float u, v, w, area;
+
 	area = cross(tmp_a, tmp_b, tmp_c);
-	for (int i = roundFloat(tmp_a.y); i <= roundFloat(tmp_b.y); i++)
+	upTrianglePixelsLeft = Brasenham(tmp_a, tmp_b);
+	upTrianglePixelsRight = Brasenham(tmp_a, tmp_d);
+	std::cout << "1. tmp_a: {" << tmp_a.x << ", " << tmp_a.y << "}\n";
+	std::cout << "1. tmp_b: {" << tmp_b.x << ", " << tmp_b.y << "}\n";
+	std::cout << "1. tmp_d: {" << tmp_d.x << ", " << tmp_d.y << "}\n";
+	if (tmp_a.x != tmp_d.x || tmp_a.y != tmp_d.y)
 	{
-		if (i < 0 || i > height) continue;
-		tmpX = tmp_a.x + slope1 * (i + 0.5 - tmp_a.y);
-		tmpX2 = tmp_a.x + slope2 * (i + 0.5 - tmp_a.y);
-		for (int j = min(roundFloat(tmpX), roundFloat(tmpX2)); j <= max(roundFloat(tmpX), roundFloat(tmpX2)); j++)
-		{	
-			if (j < 0 || j >= width) continue;
-			Vector4 p(j, i, 0, 0);
-			u = cross(tmp_b, tmp_c, p) / area;
-			v = cross(tmp_c, tmp_a, p) / area;
-			w = 1 - u - v;
-			Vertex fragment = Vertex::mix(a, b, c, u, v, w);
-			if (depthTest(i, j, fragment) == false) continue;
-			shader->fragment(fragment, this->target->pixelColor(j, i));
+		for (int i = 0; i < int(upTrianglePixelsLeft.size()); i++)
+		{
+			int start, end;
+			if (upTrianglePixelsLeft[i].x <= upTrianglePixelsRight[i].x)
+			{
+				start = upTrianglePixelsLeft[i].x;
+				end = upTrianglePixelsRight[i].x;
+			}
+			else
+			{
+				start = upTrianglePixelsRight[i].x;
+				end = upTrianglePixelsLeft[i].x;
+			}
+			for (int j = start; j <= end; j++)
+			{
+				Vector4 p(j, i, 0, 0);
+				u = cross(tmp_b, tmp_c, p) / area;
+				v = cross(tmp_c, tmp_a, p) / area;
+				w = 1 - u - v;
+				Vertex fragment = Vertex::mix(a, b, c, u, v, w);
+				if (depthTest(upTrianglePixelsLeft[i].y, j, fragment) == false) continue;
+				shader->fragment(fragment, this->target->pixelColor(j, upTrianglePixelsLeft[i].y));
+			}
 		}
 	}
-
-	slope1 = (tmp_c.x - tmp_b.x) / (tmp_c.y - tmp_b.y);
-	tmpX = tmp_c.x;
-	tmpX2 = tmpX;
-	for (int i = roundFloat(tmp_c.y); i >= roundFloat(tmp_b.y); i--)
+	if (tmp_d.x == tmp_c.x)
+		return;
+	std::cout << "2. tmp_c: {" << tmp_c.x << ", " << tmp_c.y << "}\n";
+	std::cout << "2. tmp_b: {" << tmp_b.x << ", " << tmp_b.y << "}\n";
+	std::cout << "2. tmp_d: {" << tmp_d.x << ", " << tmp_d.y << "}\n";
+	downTrianglePixelsLeft = Brasenham(tmp_c, tmp_b);
+	downTrianglePixelsRight = Brasenham(tmp_c, tmp_d);
+	std::cout << downTrianglePixelsLeft.size() << ' ' << downTrianglePixelsRight.size() << std::endl;
+	for (int i = 0; i < int(downTrianglePixelsLeft.size()); i++)
 	{
-		if (i < 0 || i > height) continue;
-		tmpX = tmp_c.x + slope1 * (i + 0.5 - tmp_c.y);
-		tmpX2 = tmp_c.x + slope2 * (i + 0.5 - tmp_c.y);
-		for (int j = min(roundFloat(tmpX), roundFloat(tmpX2)); j <= max(roundFloat(tmpX), roundFloat(tmpX2)); j++)
+		int start, end;
+		if (downTrianglePixelsLeft[i].x <= downTrianglePixelsRight[i].x)
 		{
-			if (j < 0 || j >= width) continue;
-			Vector4 p(j, i, 0, 0);
+			start = downTrianglePixelsLeft[i].x;
+			end = downTrianglePixelsRight[i].x;
+		}
+		else
+		{
+			start = downTrianglePixelsRight[i].x;
+			end = downTrianglePixelsLeft[i].x;
+		}
+		for (int j = start; j <= end; j++)
+		{
+			Vector4 p(j, downTrianglePixelsLeft[i].y, 0, 0);
 			u = cross(tmp_b, tmp_c, p) / area;
 			v = cross(tmp_c, tmp_a, p) / area;
 			w = 1 - u - v;
 			Vertex fragment = Vertex::mix(a, b, c, u, v, w);
-			if (depthTest(i, j, fragment) == false) continue;
-			shader->fragment(fragment, this->target->pixelColor(j, i));
+			if (depthTest(downTrianglePixelsLeft[i].y, j, fragment) == false) continue;
+			shader->fragment(fragment, this->target->pixelColor(j, downTrianglePixelsLeft[i].y));
 		}
 	}
 }
+
+// void StandardRasterizer::drawTriangle(Vertex &a, Vertex &b, Vertex &c, Shader *shader)
+// {
+// 	std::vector<Vertex> vec;
+// 	std::vector<Vector4> upTrianglePixelsLeft, upTrianglePixelsRight;
+// 	std::vector<Vector4> downTrianglePixelsLeft, downTrianglePixelsRight;
+// 	vec.push_back(a);
+// 	vec.push_back(b);
+// 	vec.push_back(c);
+// 	sort(vec.begin(), vec.end(), cmp);
+
+// 	Vector4 tmp_a = toScreenSpace(vec[0].position, this->width, this->height);
+// 	Vector4 tmp_b = toScreenSpace(vec[1].position, this->width, this->height);
+// 	Vector4 tmp_c = toScreenSpace(vec[2].position, this->width, this->height);
+// 	float u, v, w, area;
+
+// 	area = cross(tmp_a, tmp_b, tmp_c);
+// 	// std::cout << "1. tmp_a: {" << tmp_a.x << ", " << tmp_a.y << "}\n";
+// 	// std::cout << "1. tmp_b: {" << tmp_b.x << ", " << tmp_b.y << "}\n";
+// 	// std::cout << "1. tmp_c: {" << tmp_c.x << ", " << tmp_c.y << "}\n";
+// 	// std::cout << "upLeftSize: " << upTrianglePixelsLeft.size() << ", upRightSize: " << upTrianglePixelsRight.size() << ", downLeftSize: " << downTrianglePixelsLeft.size() << std::endl;
+// 	upTrianglePixelsLeft = Brasenham(tmp_a, tmp_b);
+// 	upTrianglePixelsRight = Brasenham(tmp_a, tmp_c);
+// 	downTrianglePixelsLeft = Brasenham(tmp_b, tmp_c);
+// 	for (int i = 0; i < int(upTrianglePixelsRight.size()); i++)
+// 	{
+// 		int start, end;
+// 		if (i < int(upTrianglePixelsLeft.size()))
+// 		{
+// 			if (upTrianglePixelsLeft[i].x <= upTrianglePixelsRight[i].x)
+// 			{
+// 				start = upTrianglePixelsLeft[i].x;
+// 				end = upTrianglePixelsRight[i].x;
+// 			}
+// 			else
+// 			{
+// 				start = upTrianglePixelsRight[i].x;
+// 				end = upTrianglePixelsLeft[i].x;
+// 			}
+// 		}
+// 		else
+// 		{
+// 			if (downTrianglePixelsLeft[i - int(upTrianglePixelsLeft.size())].x <= upTrianglePixelsRight[i].x)
+// 			{
+// 				start = downTrianglePixelsLeft[i - int(upTrianglePixelsLeft.size())].x;
+// 				end = upTrianglePixelsRight[i].x;
+// 			}
+// 			else
+// 			{
+// 				start = upTrianglePixelsRight[i].x;
+// 				end = downTrianglePixelsLeft[i - int(upTrianglePixelsLeft.size())].x;
+// 			}
+// 		}
+// 		std::cout << start << ' ' << end << std::endl;
+// 		for (int j = start + 1; j <= end; j++)
+// 		{
+// 			Vector4 p(j, upTrianglePixelsRight[i].y, 0, 0);
+// 			u = cross(tmp_b, tmp_c, p) / area;
+// 			v = cross(tmp_c, tmp_a, p) / area;
+// 			w = 1 - u - v;
+// 			Vertex fragment = Vertex::mix(a, b, c, u, v, w);
+// 			if (depthTest(upTrianglePixelsRight[i].y, j, fragment) == false) continue;
+// 			shader->fragment(fragment, this->target->pixelColor(j, upTrianglePixelsRight[i].y));
+// 		}
+// 	}
+// }
+
+// void StandardRasterizer::drawTriangle(Vertex &a, Vertex &b, Vertex &c, Shader *shader)
+// {
+// 	std::vector<Vertex> vec;
+// 	vec.push_back(a);
+// 	vec.push_back(b);
+// 	vec.push_back(c);
+// 	sort(vec.begin(), vec.end(), cmp);
+
+// 	Vector4 tmp_a = toScreenSpace(vec[0].position, this->width, this->height);
+// 	Vector4 tmp_b = toScreenSpace(vec[1].position, this->width, this->height);
+// 	Vector4 tmp_c = toScreenSpace(vec[2].position, this->width, this->height);
+// 	float slope1 = (tmp_a.x - tmp_b.x) / (tmp_a.y - tmp_b.y);
+// 	float slope2 = (tmp_a.x - tmp_c.x) / (tmp_a.y - tmp_c.y);
+
+// 	float tmpX = tmp_a.x;
+// 	float tmpX2 = tmpX;
+
+// 	float u, v, w, area;
+// 	area = cross(tmp_a, tmp_b, tmp_c);
+// 	if (tmp_a.y != tmp_b.y)
+// 	{
+// 		for (int i = roundFloat(tmp_a.y); i < roundFloat(tmp_b.y); i++)
+// 		{
+// 			if (i < 0 || i > height) continue;
+// 			tmpX = tmp_a.x + slope1 * (i + 0.5 - tmp_a.y);
+// 			tmpX2 = tmp_a.x + slope2 * (i + 0.5 - tmp_a.y);
+// 			for (int j = min(roundFloat(tmpX), roundFloat(tmpX2)); j < max(roundFloat(tmpX), roundFloat(tmpX2)); j++)
+// 			{	
+// 				if (j < 0 || j >= width) continue;
+// 				Vector4 p(j, i, 0, 0);
+// 				u = cross(tmp_b, tmp_c, p) / area;
+// 				v = cross(tmp_c, tmp_a, p) / area;
+// 				w = 1 - u - v;
+// 				Vertex fragment = Vertex::mix(a, b, c, u, v, w);
+// 				if (depthTest(i, j, fragment) == false) continue;
+// 				shader->fragment(fragment, this->target->pixelColor(j, i));
+// 			}
+// 		}
+// 	}
+
+// 	slope1 = (tmp_c.x - tmp_b.x) / (tmp_c.y - tmp_b.y);
+// 	// std::cout << "slope3: " << slope1 << std::endl;
+// 	tmpX = tmp_c.x;
+// 	tmpX2 = tmpX;
+// 	if (tmp_c.y == tmp_b.y)
+// 		return;
+// 	for (int i = roundFloat(tmp_c.y); i >= roundFloat(tmp_b.y); i--)
+// 	{
+// 		if (i < 0 || i > height) continue;
+// 		tmpX = tmp_c.x + slope1 * (i + 0.5 - tmp_c.y);
+// 		tmpX2 = tmp_c.x + slope2 * (i + 0.5 - tmp_c.y);
+// 		for (int j = min(roundFloat(tmpX), roundFloat(tmpX2)); j < max(roundFloat(tmpX), roundFloat(tmpX2)); j++)
+// 		{
+// 			if (j < 0 || j >= width) continue;
+// 			Vector4 p(j, i, 0, 0);
+// 			u = cross(tmp_b, tmp_c, p) / area;
+// 			v = cross(tmp_c, tmp_a, p) / area;
+// 			w = 1 - u - v;
+// 			Vertex fragment = Vertex::mix(a, b, c, u, v, w);
+// 			if (depthTest(i, j, fragment) == false) continue;
+// 			shader->fragment(fragment, this->target->pixelColor(j, i));
+// 		}
+// 	}
+// }
 
 StandardRasterizer::StandardRasterizer(int width, int height)
 {
@@ -126,6 +379,7 @@ StandardRasterizer::StandardRasterizer(int width, int height)
 StandardRasterizer::~StandardRasterizer()
 {
 }
+
 void	StandardRasterizer::setTarget(RenderTexture *rt) {
 	this->target = rt;
 }
@@ -141,6 +395,8 @@ inline Vertex toNDC(Vertex clip_space)
 
 void StandardRasterizer::drawPolygon(std::queue<Vertex> &polygon, Shader *shader)
 {
+	if (polygon.empty())
+		return ;
 	Vertex root = toNDC(polygon.front());
 	polygon.pop();
 	Vertex prev = toNDC(polygon.front());
@@ -150,7 +406,6 @@ void StandardRasterizer::drawPolygon(std::queue<Vertex> &polygon, Shader *shader
 	{
 		curr = toNDC(polygon.front());
 		polygon.pop();
-		std::cout << toScreenSpace(root.position, width, height).x << ", " << toScreenSpace(prev.position, width, height).x << ", " << toScreenSpace(curr.position, width, height).x << std::endl;
 		drawTriangle(root, prev, curr, shader);
 		prev = curr;
 	}
@@ -165,7 +420,7 @@ void StandardRasterizer::draw(Mesh &mesh, int count, Shader *shader, Clipper *cl
 		return ;
 	for (int i = 0; i + 3 <= count; i += 3)
 	{
-		for (int j = i; j < i + 3; j++)
+		for (int j = 0; j < 3; j++)
 		{
 			shader->vertex(mesh[i + j], tmp);
 			polygon.push(tmp);
